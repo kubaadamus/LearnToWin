c1b8564cd648d7fd110f74d378c14f702755a90f
kubaadamus <jakubadamus1991@gmail.com>
2018-12-28 15:34:05 +0100

    Jest czysta postać, teraz trzeba zaimplementować kupowanie ekwipunku

--- database_update.php
+++ database_update.php
@@ -1,5 +1,6 @@
 <?php
 
+//=================================================== PODŁĄCZENIE DO BAZY DANYCH =============================================//
 $user = 'jakubadamus';
 $DBpassword = 'Kubaadamus1991';
 $db = 'jakubadamus';
@@ -19,98 +20,14 @@ if ($database) {
 }
 echo "<br>";
 
-class Base
-{
-   public $base_id = 0;
-   public $base_name = 0;
-   public $base_price = 0;
-   public $base_defence = 0;
-   public $base_thumbnail = 0;
- 
-   public function __construct($id=0, $name=0,$price=0,$defence=0,$thumbnail=0)
-   {
-      $this->base_id = $id;
-      $this->base_name = $name;
-      $this->base_price = $price;
-      $this->base_defence = $defence;
-      $this->base_thumbnail = $thumbnail;
-      $this->wypisz();
-   }
-   public function wypisz(){
-       debug_to_console("Stworzono obiekt typu base.");
-       debug_to_console("id:".$this->base_id);
-       debug_to_console("name:".$this->base_name);
-       debug_to_console("price:".$this->base_price);
-       debug_to_console("defence:".$this->base_defence);
-       debug_to_console("thumbnail:".$this->base_thumbnail);
-       debug_to_console("--------------------------------");
-   }
-}
-class Helmet
-{
-   public $helmet_id = 0;
-   public $helmet_name = 0;
-   public $helmet_price = 0;
-   public $helmet_defence = 0;
-   public $helmet_thumbnail = 0;
- 
-   public function __construct($id=0, $name=0,$price=0,$defence=0,$thumbnail=0)
-   {
-      $this->helmet_id = $id;
-      $this->helmet_name = $name;
-      $this->helmet_price = $price;
-      $this->helmet_defence = $defence;
-      $this->helmet_thumbnail = $thumbnail;
-      $this->wypisz();
-   }
-   public function wypisz(){
-       debug_to_console("Stworzono obiekt typu helmet.");
-       debug_to_console("id:".$this->helmet_id);
-       debug_to_console("name:".$this->helmet_name);
-       debug_to_console("price:".$this->helmet_price);
-       debug_to_console("defence:".$this->helmet_defence);
-       debug_to_console("thumbnail:".$this->helmet_thumbnail);
-       debug_to_console("--------------------------------");
-   }
-}
-class Torso
-{
-   public $torso_id = 0;
-   public $torso_name = 0;
-   public $torso_price = 0;
-   public $torso_defence = 0;
-   public $torso_thumbnail = 0;
- 
-   public function __construct($id=0, $name=0,$price=0,$defence=0,$thumbnail=0)
-   {
-      $this->torso_id = $id;
-      $this->torso_name = $name;
-      $this->torso_price = $price;
-      $this->torso_defence = $defence;
-      $this->torso_thumbnail = $thumbnail;
-      $this->wypisz();
-   }
-   public function wypisz(){
-       debug_to_console("Stworzono obiekt typu torso.");
-       debug_to_console("id:".$this->torso_id);
-       debug_to_console("name:".$this->torso_name);
-       debug_to_console("price:".$this->torso_price);
-       debug_to_console("defence:".$this->torso_defence);
-       debug_to_console("thumbnail:".$this->torso_thumbnail);
-       debug_to_console("--------------------------------");
-   }
-}
+$coins=0;
 
 
-
-
-//Współczynniki ocen
+//=================================================== UPDATE MONET NA PODSTAWIE OCEN =============================================//
 $mat=5;
 $fiz=3;
 $pl=2;
-$coins=0;
 
-//Sprawdzenie legitności logującego się ucznia i pobranie rzeczy
 $sql = "SELECT * FROM uczniowie WHERE imie = '".$login."' AND nazwisko = '".$password."'" ;
 $result = mysqli_query($database,$sql);
 $success = mysqli_num_rows($result);
@@ -118,24 +35,15 @@ if($success>0)
 {
     while ($row=mysqli_fetch_row($result))
     {
-    $id = $row[0];
-    $ranga = $row[11];
-    $baza = $row[13];
-    $helm = $row[14];
-    $maska = $row[15];
-    $rekawice = $row[16];
-    $torso = $row[17];
-    $spodnie = $row[18];
-    $buty = $row[19];
-    $bron = $row[20];
-    $apteczka = $row[21];
-    $granaty = $row[22];
-    $secondary = $row[23];
-    $spec1 = $row[24];
-    $spec2 = $row[25];
-    $spec3 = $row[26];
+    $id =               $row[0];
+    $nazwa_szkoly =     $row[1];
+    $imie =             $row[2];
+    $nazwisko =         $row[3];
+    $klasa =            $row[4];
     }
     echo "<h1>Witaj ".$login." ".$password."</h1>";
+
+
     $sql = "SELECT * FROM oceny WHERE uczen_ID = '".$id."'" ;
     $result = mysqli_query($database,$sql);
     while ($row=mysqli_fetch_row($result))
@@ -156,51 +64,90 @@ if($success>0)
     $sql_updateCoins = "UPDATE uczniowie SET coins = $coins WHERE imie = '".$login."' AND nazwisko = '".$password."'" ;
     mysqli_query($database, $sql_updateCoins); //Tutaj baza danych otrzymuje aktualną wartość coins
 
-
-        //zapytaj o baze
-        $sql = "SELECT * FROM bases WHERE base_ID = '".$baza."'" ;
-        $result = mysqli_query($database,$sql);
-        while ($row=mysqli_fetch_row($result))
-        {
-            $current_base = new Base($row[0],$row[1],$row[2],$row[3],$row[4]);
-
-            foreach ($row as $value) { 
-                //echo "<td> " . $value . "</td>"; 
-                
-            }
-            echo "<br>";
-        }
-        //zapytaj o helm
-        $sql = "SELECT * FROM helmets WHERE helmet_ID = '".$helm."'" ;
-        $result = mysqli_query($database,$sql);
-        while ($row=mysqli_fetch_row($result))
-        {
-            $current_helmet = new Helmet($row[0],$row[1],$row[2],$row[3],$row[4]);
-            foreach ($row as $value) { 
-                //echo "<td> " . $value . "</td>"; 
-                
-            }
-            echo "<br>";
-        }
-        //zapytaj o torso
-        $sql = "SELECT * FROM torsos WHERE torso_ID = '".$torso."'" ;
-        $result = mysqli_query($database,$sql);
-        while ($row=mysqli_fetch_row($result))
-        {
-            $current_torso = new Torso($row[0],$row[1],$row[2],$row[3],$row[4]);
-            foreach ($row as $value) { 
-                //echo "<td> " . $value . "</td>"; 
-                
-            }
-            echo "<br>";
-        }
-
-
+    echo "<br>Twoje oceny zostały zaktualizowane. masz ".$coins." monet.";
 }
 else{
     printRow("NIEPRAWIDŁOWA NAZWA UŻYTKOWNIKA LUB HASŁO, SPRÓBUJ PONOWNIE");
 }
 
+//=================================================== STWORZENIE OBIEKTU UCZNIA =============================================//
+
+class uczen_object
+{
+
+   //Ekwipunek
+   public $base = 0;
+   public $helmet = 0;
+   public $torso = 0;
+   public $gloves = 0;
+   public $pants = 0;
+   public $boots = 0;
+   public $weapon = 0;
+
+ 
+   public function __construct($_base=0, $_helmet=0, $_torso=0, $_gloves=0, $_pants=0, $_boots=0, $_weapon=0)
+   {
+      //Ekwipunek
+      $this->base = $_base;
+      $this->helmet = $_helmet;
+      $this->torso = $_torso;
+      $this->gloves = $_gloves;
+      $this->pants = $_pants;
+      $this->boots = $_boots;
+      $this->weapon = $_weapon;
+      $this->wypisz();
+   }
+   public function wypisz(){
+       //Ekwipunek
+       debug_to_console("base:".$this->base);
+       debug_to_console("helmet:".$this->helmet);
+       debug_to_console("torso:".$this->torso);
+       debug_to_console("gloves:".$this->gloves);
+       debug_to_console("pants:".$this->pants);
+       debug_to_console("boots:".$this->boots);
+       debug_to_console("weapon:".$this->weapon);
+       debug_to_console("--------------------------------");
+   }
+}
+
+
+    $sql = "SELECT * FROM uczniowie WHERE imie = '".$login."' AND nazwisko = '".$password."'" ;
+    $result = mysqli_query($database,$sql);
+    $success = mysqli_num_rows($result);
+    if($success>0)
+    {
+        while ($row=mysqli_fetch_row($result))
+        {
+            $uczen_pobrany = json_decode($row[6]);
+        }
+    }
+
+echo "<br>".json_encode($uczen_pobrany);
+
+
+//============================================= OBLICZENIE AKTUALNEJ WARTOŚCI UCZNIA =======================================//
+$wartosc_postaci=0;
+$elements = array("base","helmet", "torso","gloves",);
+foreach ($elements as $value)
+{
+
+    $sql = "SELECT * FROM $value WHERE ID =".$uczen_pobrany->$value;
+    //echo $sql;
+    $result = mysqli_query($database,$sql);
+    $success = mysqli_num_rows($result);
+    if($success>0)
+    {
+        while ($row=mysqli_fetch_row($result))
+        {
+            echo "cena $value: ".$row[2];
+            $wartosc_postaci += $row[2];
+        }
+    }
+}
+
+echo "wartosc postaci: ".$wartosc_postaci;
+
+echo "<br> Możesz wydać: ".($coins-$wartosc_postaci);
 
 
 
@@ -213,8 +160,6 @@ function debug_to_console( $data ) {
         $output = implode( ',', $output);
     echo "<script>console.log( 'Debug Objects: " . $output . "' );</script>";
 }
-
-
 
 
 ?>
--- login.php
+++ login.php
@@ -8,67 +8,11 @@
 <body>
 
 
-<form id="buyform" action="buy.php">
-<input type="text" id="login" name="login" ><br><br>
-<input type="text" id="password" name="password" ><br><br>
-<input type="text" id="cokupic" name="type" ><br><br>
-<input type="text" id="ktore" name="number" ><br><br>
-
-</form>
-
-<input type="button" obj_type="helmet" obj_number="0" value="buy helmet 0">
-<input type="button" obj_type="helmet" obj_number="1" value="buy helmet 1">
-<input type="button" obj_type="helmet" obj_number="2" value="buy helmet 2">
-<input type="button" obj_type="helmet" obj_number="3" value="buy helmet 3">
-
 
 <script>
-    var coins = parseInt("<?php echo $coins ?>");
-    var login = ("<?php echo $login ?>");
-    var password = ("<?php echo $password ?>");
-    var id = ("<?php echo $id ?>");
-    var ranga = ("<?php echo $ranga ?>");
+
 
     
-    $("input").click(function(event) { // Przy kliknięciu na kupno jakiegoś elementu submituje form buyform z odpowiednim inputem
-        //alert($(event.target).attr("obj_type"));
-        $('#cokupic').val($(event.target).attr("obj_type"));
-        $('#ktore').val($(event.target).attr("obj_number"));
-        $('#buyform').submit();
-
-    });
-
-
-$( document ).ready(function() {
-
-    $('#coins').val(coins);
-    $('#login').val(login);
-    $('#password').val(password);
-    $('#id').val("id "+id);
-    $('#ranga').val("ranga "+ranga);
-
-//BASE//
-var current_base = '<?php echo json_encode($current_base); ?>';
-var current_base_data = JSON.parse(current_base );
-//alert(current_base_data.base_name);
-
-//HELMET//
-var current_helmet = '<?php echo json_encode($current_helmet); ?>';
-var current_helmet_data = JSON.parse(current_helmet );
-//alert(current_helmet_data.helmet_name);
-
-//TORSO//
-var current_torso = '<?php echo json_encode($current_torso); ?>';
-var current_torso_data = JSON.parse(current_torso );
-//alert(current_torso_data.torso_name);
-
-
-var current_value = parseInt(current_base_data.base_price) + parseInt(current_helmet_data.helmet_price) + parseInt(current_torso_data.torso_price);
-console.log("current_price: "+current_value);
-console.log("coins: "+coins);
-
-
-});
 
 
 
