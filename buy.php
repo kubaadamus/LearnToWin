2c7f277c70e2415a2bd794bd8e2a5b55fc998e87
kubaadamus <jakubadamus1991@gmail.com>
2019-01-02 04:31:04 +0100

    DOPIERO TERAZ DZIAŁA Kupno i sprzedaż! był jeszcze jeden błąd ->item zamiast ->item w buy.php. Teraz jest miodzio

--- buy.php
+++ buy.php
@@ -106,7 +106,7 @@ $elements = array("base","helmet","torso","gloves","pants","boots","weapon");
 foreach ($elements as $value)
 {
 
-    $sql = "SELECT * FROM $value WHERE ID =".$obj->$value;
+    $sql = "SELECT * FROM $value WHERE ID =".$uczen_pobrany->$value;
     //echo $sql;
     $result = mysqli_query($database,$sql);
     $success = mysqli_num_rows($result);
