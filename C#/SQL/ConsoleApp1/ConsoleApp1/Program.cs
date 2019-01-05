af070ca0fc62aba298291bf02a6834ac11e8c55f
kubaadamus <jakubadamus1991@gmail.com>
2019-01-05 21:34:00 +0100

    Działa pytanie php-ów z C#pa w unity i odtwarzanie struktury gracza

---
+++ C#/SQL/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Web;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            string response = SendRequest("http://imprezpol.cba.pl/login.php?login=Jakub&password=Adamus");
+            Console.ReadKey();
+            if (response != null)
+            {
+                Console.WriteLine(response);
+            }
+
+
+            Console.ReadLine();
+        }
+
+         static string SendRequest(string url)
+        {
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    return client.DownloadString(new Uri(url));
+                }
+            }
+            catch (WebException ex)
+            {
+               
+                return null;
+            }
+        }
+    }
+}
---
+++ C#/SQL/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net;
+using System.Web;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            string response = SendRequest("http://imprezpol.cba.pl/user_create.php?login=Pawel&password=Adamus");
+            if (response != null)
+            {
+                //Console.WriteLine("RISPONS:");
+                Console.WriteLine(response);
+                Deserialize<object>(response);
+            }
+            else
+            {
+                Console.WriteLine("NUL XD :C");
+            }
+
+            Console.ReadLine();
+        }
+
+        static string SendRequest(string url)
+        {
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    return client.DownloadString(new Uri(url));
+                }
+            }
+            catch (WebException ex)
+            {
+
+                return null;
+            }
+        }
+
+        public static T Deserialize<T>(string json)
+        {
+            T obj = Activator.CreateInstance<T>();
+            MemoryStream ms = new MemoryStream(Encoding.Unicode.GetBytes(json));
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(obj.GetType());
+            obj = (T)serializer.ReadObject(ms);
+            ms.Close();
+            Console.WriteLine(obj.ToString());
+            return obj;
+        }
+    }
+}
