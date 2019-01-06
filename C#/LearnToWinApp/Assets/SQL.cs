c16b9a53e4a27644f705f85edd8b02d5aae941d4
kubaadamus <jakubadamus1991@gmail.com>
2019-01-06 08:36:23 +0100

    Jest podstawowe UI w Unity. Po załadowaniu Sql odpala się event updatujący UI.

--- C#/LearnToWinApp/Assets/SQL.cs
+++ C#/LearnToWinApp/Assets/SQL.cs
@@ -3,53 +3,98 @@ using UnityEngine;
 using System.Web.Script.Serialization;
 using System;
 using System.Collections.Generic;
-
+public enum ItemType { pistol, rifle, grenade, armor, ammo, medikit, perk };
 public class SQL : MonoBehaviour {
+
+    float TotalCoins = 0;
+    float CharacterValue = 0;
+    float CoinsToSpend = 0;
+    public static Uczen Character;
+    public static event Delegat SqlFinished;
+    public delegate void Delegat(object sender, EventArgs eventargs);
 
     void Start() {
 
         //rifles
-        Item m4a1 = new Item(ItemType.rifle, 120, "m4a1", "assets/m4a1");
+        Item m4a1 = new Item(ItemType.rifle, 12, "m4a1", "assets/m4a1");
         //pistols
-        Item desertEagle = new Item(ItemType.pistol, 60, "desertEagle", "assets/desertEagle");
+        Item desertEagle = new Item(ItemType.pistol, 32, "desertEagle", "assets/desertEagle");
         //throwable
-        Item F1Grenade = new Item(ItemType.grenade, 70, "F1Grenade", "assets/F1Grenade");
+        Item F1Grenade = new Item(ItemType.grenade, 10, "F1Grenade", "assets/F1Grenade");
         //medikits
         Item FirstAidKit = new Item(ItemType.medikit, 20, "FirstAidKit", "assets/firstAidKit");
         //armors
-        Item BasicArmor = new Item(ItemType.armor, 40, "BasicArmor", "assets/BasicArmor");
+        Item BasicArmor = new Item(ItemType.armor, 50, "BasicArmor", "assets/BasicArmor");
 
         //Uczen Character = new Uczen("Jakub", "Adamus", m4a1, desertEagle, F1Grenade, FirstAidKit, BasicArmor, new List<Item>());
         //Character.ItemList.Add(new Item(ItemType.rifle, 20, "aaa", "aaabbb"));
-
-
+        //Debug.Log(SQLQueryClass.SqlQuery("universal_query.php", "login=Jakub&password=Adamus&query=SELECT * FROM pants UNION SELECT * FROM torso UNION SELECT * FROM helmet")); // pobieranie z wielu tabel jednocześnie * muszą mieć te same ilości kolumn
+        //testuczen.ItemList.RemoveAll(s => s.name == "kupa"); // usuwa wszystkie itemy których name to kupa
         //var json = new JavaScriptSerializer().Serialize(Character);
         //Debug.Log(json);
 
-        
-        Uczen testuczen = new JavaScriptSerializer().Deserialize<Uczen>(SQLQueryClass.SqlQuery("user_create.php", "login=Jakub&password=Adamus&")); // pobieranie ucznia z serwera
 
-        //Debug.Log(testuczen.primary.price+=10); // jakaś operacja zmiany na uczniu
+        CharacterDownload();
+        CharacterUpload();
 
-        int wartoscUcznia = testuczen.primary.price + testuczen.secondary.price + testuczen.throwable.price + testuczen.medikit.price + testuczen.armor.price;
 
-        foreach(Item i in testuczen.ItemList)
-        {
-            wartoscUcznia += i.price;
-        }
-
-        Debug.Log("Wartość ucznia to: " + wartoscUcznia);
-        //Debug.Log(SQLQueryClass.SqlQuery("universal_query.php", "login=Jakub&password=Adamus&query=SELECT * FROM pants UNION SELECT * FROM torso UNION SELECT * FROM helmet")); // pobieranie z wielu tabel jednocześnie * muszą mieć te same ilości kolumn
-
-        string apdejt = "UPDATE uczniowie SET uczen_object ='" + new JavaScriptSerializer().Serialize(testuczen) + "' WHERE imie = 'Jakub' AND nazwisko = 'Adamus'"; // wysyłanie ucznia na serwer
-
-        Debug.Log(SQLQueryClass.SqlQuery("universal_query.php", "login=Jakub&password=Adamus&query="+apdejt+""));
 
 
     }
+    float CalculateCharacterValue(Uczen Character)
+    {
+        int _CharacterValue = Character.primary.price + Character.secondary.price + Character.throwable.price + Character.medikit.price + Character.armor.price;
 
+        foreach (Item i in Character.ItemList)
+        {
+            _CharacterValue += i.price;
+        }
 
+        return _CharacterValue;
+
+    }
+    public void CharacterDownload()
+    {
+
+        Character = new JavaScriptSerializer().Deserialize<Uczen>(SQLQueryClass.SqlQuery("user_create.php", "login=Jakub&password=Adamus&"));       // pobieranie ucznia z serwera
+        TotalCoins = float.Parse(SQLQueryClass.SqlQuery("coins_update.php", "login=Jakub&password=Adamus&").Replace(".", ","));                     // Pobierz ilość monet na podstawie ocen
+        CharacterValue = CalculateCharacterValue(Character);                                                                                        //Oblicz wartość postaci
+        CoinsToSpend = TotalCoins - CharacterValue;                                                                                                 //Obliczamy dostępne środki
+
+        Debug.Log("Totalna ilość dostępnych coinsów to: " + TotalCoins);
+        Debug.Log("Wartość ucznia to: " + CharacterValue);
+        Debug.Log("Możesz wydać: " + CoinsToSpend);
+
+        SqlFinished(this, EventArgs.Empty);
+    }
+    public void CharacterUpload()
+    {
+        string apdejt = "UPDATE uczniowie SET uczen_object ='" + new JavaScriptSerializer().Serialize(Character) + "' WHERE imie = 'Jakub' AND nazwisko = 'Adamus'"; // wysyłanie ucznia na serwer
+        SQLQueryClass.SqlQuery("universal_query.php", "login=Jakub&password=Adamus&query=" + apdejt + "");
+    }
+    void CharacterAddItem()
+    {
+        //Character.perk = (new Item(ItemType.perk, 2, "perkxDD", "assets/perklolzz"));
+        //Character.ItemList.RemoveAll(s => s.name == "ammo"); // usuwa wszystkie itemy których name to kupa
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            CharacterDownload();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            CharacterUpload();
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            CharacterAddItem();
+        }
+    }
 }
+
+
 
 public class Uczen
 {
@@ -62,6 +107,7 @@ public class Uczen
     public Item throwable;
     public Item medikit;
     public Item armor;
+    public Item perk;
 
     public List<Item> ItemList;
 
@@ -69,7 +115,8 @@ public class Uczen
     {
 
     }
-    public Uczen(string imie, string nazwisko, Item primary, Item secondary, Item throwable, Item medikit, Item armor, List<Item> itemList)
+
+    public Uczen(string imie, string nazwisko, Item primary, Item secondary, Item throwable, Item medikit, Item armor, Item perk, List<Item> itemList)
     {
         this.imie = imie;
         this.nazwisko = nazwisko;
@@ -78,6 +125,7 @@ public class Uczen
         this.throwable = throwable;
         this.medikit = medikit;
         this.armor = armor;
+        this.perk = perk;
         ItemList = itemList;
     }
 }
@@ -101,4 +149,4 @@ public class Item
     }
 }
 
-public enum ItemType {pistol, rifle, grenade, armor, ammo, medikit};
+
---
+++ C#/LearnToWinApp/Assets/UiButtonScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+public class UiButtonScript : MonoBehaviour {
+
+    public enum UiButtonType { primary, secondary, throwable, medikit, armor, perk};
+    bool once = true;
+	public UiButtonType type;
+    Button button;
+    
+	void Start () {
+
+        button = GetComponent<Button>();
+        SQL.SqlFinished += UiUpdate;
+    }
+	
+    
+	void Update () {
+
+	}
+
+    public void UiUpdate(object sender, EventArgs eventargs)
+    {
+        if (type == UiButtonType.primary && SQL.Character.primary != null)
+        {
+            button.GetComponentInChildren<Text>().text = SQL.Character.primary.name;
+        }
+
+        else if (type == UiButtonType.secondary && SQL.Character.secondary != null)
+        {
+            button.GetComponentInChildren<Text>().text = SQL.Character.secondary.name;
+        }
+        else if (type == UiButtonType.throwable && SQL.Character.throwable != null)
+        {
+            button.GetComponentInChildren<Text>().text = SQL.Character.throwable.name;
+        }
+        else if (type == UiButtonType.medikit && SQL.Character.medikit!=null)
+        {
+            button.GetComponentInChildren<Text>().text = SQL.Character.medikit.name;
+        }
+        else if (type == UiButtonType.armor && SQL.Character.armor != null)
+        {
+            button.GetComponentInChildren<Text>().text = SQL.Character.armor.name;
+        }
+        else if (type == UiButtonType.perk && SQL.Character.perk != null)
+        {
+            button.GetComponentInChildren<Text>().text = SQL.Character.perk.name;
+        }
+        else
+        {
+            button.GetComponentInChildren<Text>().text = "BRAK";
+        }
+    }
+}
